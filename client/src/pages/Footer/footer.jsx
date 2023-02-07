823ba4c8732c53cdfbada612324bfcbccaa57d96
Ramel Jay <rameljey15@gmail.com>
2023-02-07 17:39:29 +0800

    client

--- client/src/pages/Footer/footer.jsx
+++ client/src/pages/Footer/footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./footer.css";
+import { FaFacebook, FaYoutube } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 function footer() {
+
+	const navigate = useNavigate;
+
     return (
 		<div>
         <footer className="footerdesign">
+			<div className="social-media">
+				<button className="fb"><a href="https://www.facebook.com/ACLCTacCity"><FaFacebook/></a></button>
+				<button className="yt"><a href="https://www.youtube.com/@aclccollegecreatives7586/featured"><FaYoutube/></a></button>
+			</div>
 				<p>© Copyright 2021 ACLC College of Tacloban</p>	
 		</footer>
 		</div>
